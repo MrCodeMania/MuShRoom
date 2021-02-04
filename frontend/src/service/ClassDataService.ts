d46c4fd6920f8e22dc46bb5a66876f38275074b8
권연욱 <w00k0805@naver.com>
2021-02-04 14:14:06 +0900

    modify : 강의 상세 back 연동
    - src/components/class/
    	- ClassAllList.vue : 클래스 전체 조회
    	- detail/ : 해당 클래스 정보, 유저 정보 조회
    - src/views/ClassDetail.vue : 해당 클래스 정보, 유저 정보 조회
    - src/service/ClassdataService.ts : class 관련 api 추가
    - src/router/index.ts : 라우터 경로 수정
    - src/assets/mushroom.png : 사진 크기 조절

--- frontend/src/service/ClassDataService.ts
+++ frontend/src/service/ClassDataService.ts
@@ -1,6 +1,7 @@
 import http from "./axios.service";
 
 class ClassDataService {
+  // 전체 강의 조회
   getAllClass() {
     return http.get("/class");
     }
@@ -8,6 +9,18 @@ class ClassDataService {
   getOneClass(id: string) {
     return http.get(`/class/${id}`);
   }
+
+  // 특정 강의의 전체 섹션 조회
+  getAllSection(classId: string) {
+    return http.get(`/section?classId=${classId}`);
+  }
+
+  // 회원정보 수정
+  updateUserInfo(data: object) {
+    return http.post("user/updateInfo/", data);
+  }
+
+  // 특정 강의 질의응답 조회
 }
 
 export default new ClassDataService();
